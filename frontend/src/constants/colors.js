72cb13d0d8714b1cf99a2160ba630bc12184dfc5
ngovandong <dongngo2001@gmail.com>
2023-02-12 14:38:41 +0700

    Feat: add navbar

---
+++ frontend/src/constants/colors.js
@@ -0,0 +1,6 @@
+export const COLORS = {
+  PURPLE: "#4255ff",
+  APP_BACKGROUND: "#f6f7fb",
+  MAIN_TEXT: "#282e3e",
+  MINOR_TEXT: "#646f90",
+};
---
+++ frontend/src/utils/theme.js
@@ -0,0 +1,14 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    purple: {
+      main: "#4255ff",
+    },
+    white: {
+      white: "#fff",
+    },
+  },
+});
+
+export default theme;
